257545b6189684e711450ab17a3d63e5dbc2e04a
vinnione <alexkir90@gmail.com>
2022-02-01 19:28:53 +0300

    add newValue

--- task_1/solution.js
+++ task_1/solution.js
@@ -23,6 +23,10 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue + 1;
+    
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
