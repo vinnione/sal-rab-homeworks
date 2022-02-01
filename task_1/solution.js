91c536154129b7cd27ee0145d5666da34b264f1e
vinnione <alexkir90@gmail.com>
2022-02-01 20:06:38 +0300

    add fix v0.2

--- task_1/solution.js
+++ task_1/solution.js
@@ -9,7 +9,7 @@ function addInCartMessage(title, price) {
     message = message + " ";
     message = message + productPrice;
     message = message + " ";
-    message = message + "теперь в корзине";
+    message = message + "теперь в корзине!";
 
     console.log(message);
 
